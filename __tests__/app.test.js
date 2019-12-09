15f62cdcfaecfe10edbd6c2eaa45c9fcb793d1b9
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-09 15:46:29 -0800

    test app client

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,19 @@
 const request = require('supertest');
+const net = require('net');
 const app = require('../lib/app');
 
-describe('createResponse', () => {
+describe('app client', () => {
+  it('sends "hi" on / ', () => {
+    const client = new net.Socket();
+    client.write(`GET / HTTP/1.1
+    Accept-Language: en-us`);
+    client.on('data', (response) => {
+      expect(response).toEqual(`HTTP/1.1 200 OK
+Accept-Ranges: bytes
+Content-Length: 2
+Content-Type: text/plain
 
+hi`);
+    });
+  });
 });
