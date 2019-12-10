8684eefe6741777d6ea29b104416cfc1a5d1aeb4
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-09 17:17:42 -0800

    I have tests but I should have used supertest, so I'm going to tear them out.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,21 +1,32 @@
 const request = require('supertest');
 const net = require('net');
 const app = require('../lib/app');
+const client = require('../lib/client');
 
 describe('client connection', () => {
-  it('sends "hi" on / ', () => {
-    const client = new net.Socket();
-    client.connect(8001, () => {
-      client.write(`GET / HTTP/1.1
-      Accept-Language: en-us`);
-      client.on('data', (response) => {
-        expect(response.toString()).toEqual(`HTTP/1.1 200 OK
+  it('sends "hi" on GET /', () => {
+    client({ method: 'GET', path: '/' }, data => {
+      expect(data.toString()).toEqual(`HTTP/1.1 200 OK
 Accept-Ranges: bytes
 Content-Length: 2
 Content-Type: text/plain
 
 hi`);
-      });
     });
   });
+
+  it('sends html on GET /green', () => {
+    client({ method: 'GET', path: '/green' }, data => {
+      expect(data.toString()).toEqual(`HTTP/1.1 200 OK
+Accept-Ranges: bytes
+Content-Length: 46
+Content-Type: text/html
+
+<html><head></head><body><h1>green</h1></body>`);
+    });
+  });
+
+  it('echoes request body on POST /echo', () => {
+    client({ method: POST, path: 'echo', body:  } () => )
+  })
 });
