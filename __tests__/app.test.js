74dde17ce99306c5e6bcac942d2e8708de703b6f
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-09 21:04:53 -0800

    use response.text instead of response.body, added post test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,32 +1,25 @@
 const request = require('supertest');
-const net = require('net');
 const app = require('../lib/app');
-const client = require('../lib/client');
 
 describe('client connection', () => {
   it('sends "hi" on GET /', () => {
-    client({ method: 'GET', path: '/' }, data => {
-      expect(data.toString()).toEqual(`HTTP/1.1 200 OK
-Accept-Ranges: bytes
-Content-Length: 2
-Content-Type: text/plain
-
-hi`);
-    });
+    return request(app).get('/')
+      .then(response => {
+        expect(response.status).toBe(200);
+        expect(response.text).toBe('hi');
+      });
   });
 
-  it('sends html on GET /green', () => {
-    client({ method: 'GET', path: '/green' }, data => {
-      expect(data.toString()).toEqual(`HTTP/1.1 200 OK
-Accept-Ranges: bytes
-Content-Length: 46
-Content-Type: text/html
-
-<html><head></head><body><h1>green</h1></body>`);
-    });
+  it('sends html on GET /green', async done => {
+    const response = await request(app).get('/green');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('<html><head></head><body><h1>green</h1></body>');
+    done();
   });
 
-  it('echoes request body on POST /echo', () => {
+  it('echoes request body on POST /echo', async done => {
+    const response = await request(app).post('/echo', { method: POST, path: 'echo', body:  })
     client({ method: POST, path: 'echo', body:  } () => )
+    done();
   })
 });
