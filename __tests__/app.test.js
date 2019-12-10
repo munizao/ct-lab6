c9d69b6aa4d97c13ce7ca2e3128e8c2805678498
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-09 16:06:23 -0800

    turn the buffer to a string

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,18 +2,20 @@ const request = require('supertest');
 const net = require('net');
 const app = require('../lib/app');
 
-describe('app client', () => {
+describe('client connection', () => {
   it('sends "hi" on / ', () => {
     const client = new net.Socket();
-    client.write(`GET / HTTP/1.1
-    Accept-Language: en-us`);
-    client.on('data', (response) => {
-      expect(response).toEqual(`HTTP/1.1 200 OK
+    client.connect(8001, () => {
+      client.write(`GET / HTTP/1.1
+      Accept-Language: en-us`);
+      client.on('data', (response) => {
+        expect(response.toString()).toEqual(`HTTP/1.1 200 OK
 Accept-Ranges: bytes
 Content-Length: 2
 Content-Type: text/plain
 
 hi`);
+      });
     });
   });
 });
